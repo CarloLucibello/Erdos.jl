cce40ca8597c090b974ae77133d07ce9a03a95b8
CarloLucibello <carlo.lucibello@gmail.com>
2017-01-13 10:11:44 +0100

    add missin constructor

--- test/factory/graph.jl
+++ test/factory/graph.jl
@@ -1,1 +1,10 @@
 #tests for the concrete types Graph and DiGraph
+g = DiGraph(10, 20)
+@test  nv(g) == 10
+@test  ne(g) == 20
+@test typeof(g) == DiGraph{Int}
+
+g = Graph(10, 20)
+@test  nv(g) == 10
+@test  ne(g) == 20
+@test typeof(g) == Graph{Int}
