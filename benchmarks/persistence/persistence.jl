a2802c90f1fe01a4629eaedea26d6c2543b32136
CarloLucibello <carlo.lucibello@gmail.com>
2017-02-25 20:37:23 +0100

    update benchs

--- benchmarks/persistence/persistence.jl
+++ benchmarks/persistence/persistence.jl
@@ -1,22 +1,23 @@
 s = BenchmarkGroup()
 suite["persistence"] = s
 
-fname, fio = mktemp()
-close(fio)
+fname = joinpath(bench_dir,"data","benchgraph_")
 srand(17)
 for G in GLIST
-    for n in [10000]
+    for n in [100]
         g = G(n, 10n, seed=1)
-        # s["write","gt","$g"] = @benchmarkable writegraph($(fname*".gt"), $g)
-        # s["write","net","$g"] = @benchmarkable writegraph($(fname*".net"), $g)
-        s["write","gml","$g"] = @benchmarkable writegraph($(fname*".gml"), $g)
-        # s["write","graphml","$g"] = @benchmarkable writegraph($(fname*".graphml"), $g)
-        # s["write","gexf","$g"] = @benchmarkable writegraph($(fname*".gexf"), $g)
-        #
-        # s["read","gt","$g"] = @benchmarkable readgraph($(fname*".gt"), $G)
-        # s["read","net","$g"] = @benchmarkable readgraph($(fname*".net"), $G)
-        s["read","gml","$g"] = @benchmarkable readgraph($(fname*".gml"), $G)
-        # s["read","graphml","$g"] = @benchmarkable readgraph($(fname*".graphml"), $G)
-        # s["read","gexf","$g"] = @benchmarkable readgraph($(fname*".gexf"), $G)
+        s["write","gt","$g"] = @benchmarkable writegraph($(fname*"$n.gt"), $g)
+        s["write","net","$g"] = @benchmarkable writegraph($(fname*"$n.net"), $g)
+        s["write","gml","$g"] = @benchmarkable writegraph($(fname*"$n.gml"), $g)
+        s["write","dot","$g"] = @benchmarkable writegraph($(fname*"$n.dot"), $g)
+        s["write","graphml","$g"] = @benchmarkable writegraph($(fname*"$n.graphml"), $g)
+        s["write","gexf","$g"] = @benchmarkable writegraph($(fname*"$n.gexf"), $g)
+
+        s["read","gt","$g"] = @benchmarkable readgraph($(fname*"$n.gt"), $G)
+        s["read","net","$g"] = @benchmarkable readgraph($(fname*"$n.net"), $G)
+        s["read","gml","$g"] = @benchmarkable readgraph($(fname*"$n.gml"), $G) #SLOW
+        s["read","dot","$g"] = @benchmarkable readgraph($(fname*"$n.dot"), $G) #SLOW
+        s["read","graphml","$g"] = @benchmarkable readgraph($(fname*"$n.graphml"), $G)
+        s["read","gexf","$g"] = @benchmarkable readgraph($(fname*"$n.gexf"), $G)
     end
 end
