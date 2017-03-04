9ffeccc189a08d8b21d7bb8b15b19bdd2118dd1f
CarloLucibello <carlo.lucibello@gmail.com>
2017-03-04 01:01:45 +0100

    update Compat require

--- docs/make.jl
+++ docs/make.jl
@@ -7,5 +7,5 @@ makedocs(modules=[Erdos], doctest = true)
 deploydocs(
     deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     repo   = "github.com/CarloLucibello/Erdos.jl.git",
-    julia  = "release"
+    julia  = "0.5"
     )
