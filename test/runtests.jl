13706d7d9b93a06778f075101d72f6077d73f1b1
CarloLucibello <carlo.lucibello@gmail.com>
2017-03-16 09:48:36 +0100

    enable all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -61,8 +61,8 @@ tests = [
 ]
 
 GLIST =    [
-            # (Graph{Int64}, DiGraph{Int64}),
-            # (Graph{UInt32}, DiGraph{UInt32}),
+            (Graph{Int64}, DiGraph{Int64}),
+            (Graph{UInt32}, DiGraph{UInt32}),
             (Net, DiNet)
             ]
 
