4452d079846afa4881dda93c0cbc2a0e4226908d
CarloLucibello <carlo.lucibello@gmail.com>
2017-06-18 18:17:04 +0200

    update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,27 +6,18 @@ using Erdos
 # Getting Started page in docs, to avoid duplication.
 if VERSION < v"0.6"
     readme = readlines("../README.md")
-    idx_features = findfirst(readme, "## Features\n") + 1
     indexbase = readlines("src/indexbase.md")
-    open("src/index.md", "w") do f
-        for l in indexbase
-            print(f, l)
-        end
-        for l in readme[idx_features:end]
-            print(f, l)
-        end
+else
+    readme = readlines("../README.md", chomp=false)
+    indexbase = readlines("src/indexbase.md", chomp=false)
+end
+idx_features = findfirst(readme, "## Features") + 1
+open("src/index.md", "w") do f
+    for l in indexbase
+        println(f, l)
     end
-else # chomped in julia 0.6
-    readme = readlines("../README.md")
-    idx_features = findfirst(readme, "## Features") + 1
-    indexbase = readlines("src/indexbase.md")
-    open("src/index.md", "w") do f
-        for l in indexbase
-            println(f, l)
-        end
-        for l in readme[idx_features:end]
-            println(f, l)
-        end
+    for l in readme[idx_features:end]
+        println(f, l)
     end
 end
 
@@ -37,5 +28,5 @@ makedocs(modules=[Erdos], doctest = true)
 deploydocs(
     deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     repo   = "github.com/CarloLucibello/Erdos.jl.git",
-    julia  = "0.5"
+    julia  = "0.6"
     )
